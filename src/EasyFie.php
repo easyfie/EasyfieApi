29b099cfa5e773b207c5573bac81c76de1d75126
Touhidul Ahmed Robi <10785280+touhid12310@users.noreply.github.com>
2022-03-01 17:46:26 +0600

    Update Json Orders

--- src/EasyFie.php
+++ src/EasyFie.php
@@ -303,6 +303,8 @@ class EasyFie
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             $data = curl_exec($ch);
             curl_close($ch);
+            
+            return json_decode($data);
         }
     }
 }
